4bb045812a1b376c1072ddd528f8c086bcae7596
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-07-30 21:15:51 +0900

    Add dist

---
+++ dist/TileImage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function importAll(r) {
+  var images = new Map();
+  r.keys().forEach(function (item, _index) {
+    return images.set(item.replace('./', '').replace('.gif', ''), r(item).default);
+  });
+  return images;
+}
+
+var images = importAll(require.context('./images', false, /\.(gif|png|jpe?g|svg)$/));
+
+var TileImage = function TileImage(_ref) {
+  var tile = _ref.tile;
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("img", {
+    src: images.get(tile),
+    alt: "nope"
+  }));
+};
+
+var _default = TileImage;
+exports.default = _default;
