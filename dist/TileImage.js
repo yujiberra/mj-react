98cdd2262fa641c0d89df7138f6929f7f364505c
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-07-30 21:53:52 +0900

    Set new JSX transform in babel config

--- dist/TileImage.js
+++ dist/TileImage.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _jsxRuntime = require("react/jsx-runtime");
+
 function importAll(r) {
   var images = new Map();
   r.keys().forEach(function (item, _index) {
@@ -17,10 +19,12 @@ var images = importAll(require.context('./images', false, /\.(gif|png|jpe?g|svg)
 
 var TileImage = function TileImage(_ref) {
   var tile = _ref.tile;
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("img", {
-    src: images.get(tile),
-    alt: "nope"
-  }));
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("img", {
+      src: images.get(tile),
+      alt: "nope"
+    })
+  });
 };
 
 var _default = TileImage;
