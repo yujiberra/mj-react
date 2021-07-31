5a26e4b7d1043f3acaac822affb614ca5eefe462
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-07-31 20:55:50 +0900

    Add TilePalette component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ClickableTile from './components/ClickableTile';
 import TileImage from './components/TileImage';
+import TilePalette from './components/TilePalette';
 
 const logTile = (tile: string) =>
   console.log(tile);
@@ -9,12 +10,14 @@ function App() {
   return (
     <div className="App">
         <h2>mj-react example</h2>
-        <h3>Non-clickable tiles</h3>
+        <h3>Tiles</h3>
         <TileImage tile="1m"></TileImage>
         <TileImage tile="1z"></TileImage>
         <h3>Clickable tiles</h3>
         <ClickableTile tile="1m" onClick={logTile}></ClickableTile>
         <ClickableTile tile="1z" onClick={logTile}></ClickableTile>
+        <h3>Tile palette</h3>
+        <TilePalette onSelectTile={logTile}></TilePalette>
     </div>
   );
 }
---
+++ src/components/TilePalette.tsx
@@ -0,0 +1,26 @@
+import { manzus, pinzus, sozus, zupais } from 'eshanten'
+import ClickableTile from './ClickableTile';
+import './TilePalette.css'
+
+interface TilePaletteProps {
+  onSelectTile: ((tile: string) => void);
+}
+
+const TilePalette = ({ onSelectTile }: TilePaletteProps) => {
+  const tileToCLickableTile = (tile: string) =>
+    <ClickableTile tile={tile} key={tile} onClick={onSelectTile} />
+
+  const manzuButtons = manzus().map(tileToCLickableTile);
+  const pinzuButtons = pinzus().map(tileToCLickableTile);
+  const sozuButtons = sozus().map(tileToCLickableTile);
+  const zupaiButtons = zupais().map(tileToCLickableTile);
+  return (
+    <div className="tile-palette">
+      {manzuButtons} <br/>
+      {pinzuButtons} <br/>
+      {sozuButtons} <br/>
+      {zupaiButtons} <br/>
+    </div>
+  );
+}
+export default TilePalette;
